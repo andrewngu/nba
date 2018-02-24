0371f6b0f76f0c8a606391aa761d2aaa39d4e510
Andrew Nguyen <andrewngu@gmail.com>
2018-02-23 20:22:50 -0800

    shuffling around player_stats

--- player_stats.py
+++ player_stats.py
@@ -1,18 +1,22 @@
+import requests
+from db import cur, db
+from utils import get_year_string
+
+
 def fetch_player_stats_by_year(year):
     year_string = get_year_string(year)
     url = 'https://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=PerGame&Scope=S&Season=' + year_string + '&SeasonType=Regular+Season&StatCategory=PTS'
     r = requests.get()
-    return r.json()
-
-def parse_player_stats(json_result):
+    json_result = r.json()
     headers = json_result['resultSet']['headers']
     rows = json_result['resultSet']['rowSet']
-
     return (headers, rows)
+
+
+def insert_player_stats_by_year(headers, rows):
+    print headers, rows
+
 
 def scape_player_stats_by_year(year):
     json_result = fetch_player_stats_by_year(year)
     parse_player_stats(json_result)
-
-def store_player_stats_by_year(headers, rows):
-    return
--- scraper.py
+++ scraper.py
@@ -1,11 +1,11 @@
 import requests
 import time
 from db import load_mysql_schema
-from team_stats import scrape_team_stats_by_year
+from player_stats import scrape_player_stats_by_year
 
 def main():
     load_mysql_schema()
-    scrape_team_stats_by_year(2017)
+    scape_player_stats_by_year(2017)
 
 if __name__ == '__main__':
     main()
